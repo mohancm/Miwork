d22e7d77eb88a55763b94f6d008fa2f810cef954
Mohan Cm <mohanmanjappa@gmail.com>
2018-08-26 01:14:52 +0530

    Lesson 1.3 + 1.4 +1.5 * Add OnclickListener for each activity * Set toast for each activity * remove onClick attribute from xml

--- app/src/main/java/com/mohan/miwork/MainActivity.java
+++ app/src/main/java/com/mohan/miwork/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +13,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        // Set a Clicklistner on that View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
+                Toast.makeText(view.getContext(),"open the list of numbers",Toast.LENGTH_SHORT).show();
+
+                // Start new Activity
+                startActivity(numbersIntent);
+            }
+        });
+
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        // Set a ClickListner on that View
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                Toast.makeText(view.getContext(),"Open the list of Colors",Toast.LENGTH_SHORT).show();
+
+                // Start the new activity
+                startActivity(colorsIntent);
+            }
+        });
+
+        TextView family = (TextView) findViewById(R.id.family);
+
+        // Set a OnClickListner on that View
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this,FamilyActivity.class);
+                Toast.makeText(view.getContext(),"Open the List of Family Members",Toast.LENGTH_SHORT).show();
+
+                // Start the FamilyActivity
+                startActivity(familyIntent);
+            }
+        });
+
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        // Set an OnClickListner for that View
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                Toast.makeText(view.getContext(),"Open the List of Phrases", Toast.LENGTH_SHORT).show();
+
+                // start PhrasesActivity
+                startActivity(phrasesIntent);
+            }
+        });
     }
 }
