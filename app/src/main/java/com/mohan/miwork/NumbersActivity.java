dd7f66ec9f42b3f163d22ffe52a48f39dc71da76
Mohan Cm <mohanmanjappa@gmail.com>
2018-08-31 03:02:52 +0530

    use for loops

--- app/src/main/java/com/mohan/miwork/NumbersActivity.java
+++ app/src/main/java/com/mohan/miwork/NumbersActivity.java
@@ -32,13 +32,7 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
-        //Create a variable to keep track of the current index posiition
-        int index = 0;
-
-        // keep looping untill we've reacher the end of the list (which means keep looping
-        // as long as the current index position is less than the length of the list)
-
-        while (index < words.size()) {
+        for (int index = 0; index < words.size(); index++){
             // Create new TextView
             TextView wordView = new TextView(this);
 
@@ -47,9 +41,6 @@ public class NumbersActivity extends AppCompatActivity {
 
             // Set the TextView as another child to the root view of this layout
             rootView.addView(wordView);
-
-            // Increment the index varible by 1
-            index++;
         }
     }
 }
