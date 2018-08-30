c5ddf1a62fdf5a1b8f43edcf247a274ba9d6716a
Mohan Cm <mohanmanjappa@gmail.com>
2018-08-31 01:53:05 +0530

    ArrayLists are better than Arrays

--- app/src/main/java/com/mohan/miwork/NumbersActivity.java
+++ app/src/main/java/com/mohan/miwork/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -12,19 +14,28 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         // Add Array of Numbers
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        // Verify the contents of the array by Printing out each element to the log
-        Log.v("NumbersActivity", "word at index 0: " + words[0]);
+        // Verify the contents of the ArrayList by printing out each element to the logs to the log
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(3));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(4));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(5));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(6));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(8));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(9));
     }
 }
