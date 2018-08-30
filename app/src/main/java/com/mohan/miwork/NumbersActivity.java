2a56ef9638200ec482b74c6f2dd8666de5e9f7fa
Mohan Cm <mohanmanjappa@gmail.com>
2018-08-31 01:47:33 +0530

    Implement Array to show numbers list in NumbersActivity

--- app/src/main/java/com/mohan/miwork/NumbersActivity.java
+++ app/src/main/java/com/mohan/miwork/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.mohan.miwork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Add Array of Numbers
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        // Verify the contents of the array by Printing out each element to the log
+        Log.v("NumbersActivity", "word at index 0: " + words[0]);
     }
 }
