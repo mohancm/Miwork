81e640788a8da292db6627dbe3a46b7194ab3e2d
Mohan Cm <mohanmanjappa@gmail.com>
2018-09-04 21:11:21 +0530

    Switch to Using ListView and ArrayAdapter

--- app/src/main/java/com/mohan/miwork/NumbersActivity.java
+++ app/src/main/java/com/mohan/miwork/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.mohan.miwork;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,17 +32,10 @@ public class NumbersActivity extends AppCompatActivity {
 
         //Find the root view so we can add child views to it
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        for (int index = 0; index < words.size(); index++){
-            // Create new TextView
-            TextView wordView = new TextView(this);
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            // Set the Text to be word at the current index
-            wordView.setText(words.get(index));
-
-            // Set the TextView as another child to the root view of this layout
-            rootView.addView(wordView);
-        }
+        listView.setAdapter(itemsAdapter);
     }
 }
