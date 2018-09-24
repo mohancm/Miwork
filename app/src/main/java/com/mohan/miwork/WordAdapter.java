194e7dfbbd41d71eb5f644193394b9ce87b40df1
Mohan Cm <mohanmanjappa@gmail.com>
2018-09-24 17:01:37 +0530

    modify WordAdapter to load image

--- app/src/main/java/com/mohan/miwork/WordAdapter.java
+++ app/src/main/java/com/mohan/miwork/WordAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -38,6 +39,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getmDefaultTranslation());
+
+        ImageView resourceImageId = (ImageView) listItemView.findViewById(R.id.image);
+        resourceImageId.setImageResource(currentWord.getmImageResourceId());
 
         return listItemView;
     }
